33bc04a234df11cf924e405326338491a14f2f0d
V-Modder <V-Modder@web.de>
2014-10-26 16:24:14 +0100

    Initial commit

---
+++ SharpUpdateDownloadForm.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace SharpUpdate
+{
+    /// <summary>
+    /// Form that download the update
+    /// </summary>
+    internal partial class SharpUpdateDownloadForm : Form
+    {
+        /// <summary>
+        /// The web client to download the update
+        /// </summary>
+        private WebClient webClient;
+
+        /// <summary>
+        /// The thread to hash the file on
+        /// </summary>
+        private BackgroundWorker bgWorker;
+
+        /// <summary>
+        /// Iterating variable in the events
+        /// </summary>
+        private int count = 0;
+
+        /// <summary>
+        /// Gets the list of alltemp files path for the downloaded files
+        /// </summary>
+        internal List<SharpUpdateFileInfo> TempFilesPath
+        {
+            get { return this.files; }
+        }
+
+        /// <summary>
+        /// Holds all paths to the tempfiles
+        /// </summary>
+        private List<SharpUpdateFileInfo> files;
+
+        /// <summary>
+        /// Creates a new SharpUpdateDownloadForm
+        /// </summary>
+        internal SharpUpdateDownloadForm(List<SharpUpdateFileInfo> files, Icon programIcon)
+        {
+            InitializeComponent();
+
+            if (programIcon != null)
+                this.Icon = programIcon;
+
+            this.files = files;
+
+            this.progressBarAll.Maximum = files.Count;
+
+            // Set the temp file name and create new 0-byte file
+            files[count].TempFile = Path.GetTempFileName();
+
+            // Set up WebClient to download file
+            webClient = new WebClient();
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(webClient_DownloadProgressChanged);
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(webClient_DownloadFileCompleted);
+
+            // Set up backgroundworker to hash file
+            bgWorker = new BackgroundWorker();
+            bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+            bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_RunWorkerCompleted);
+
+            // Download file
+            try { webClient.DownloadFileAsync(new Uri(files[count].Url), files[count++].TempFile); }
+            catch { this.DialogResult = DialogResult.No; this.Close(); }
+        }
+
+        /// <summary>
+        /// Downloads file from server
+        /// </summary>
+        private void webClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update progressbar on download
+            this.lblProgress.Text = String.Format("Downloaded {0} of {1}", FormatBytes(e.BytesReceived, 1, true), FormatBytes(e.TotalBytesToReceive, 1, true));
+            this.progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void webClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                this.DialogResult = DialogResult.No;
+                this.Close();
+            }
+            else if (e.Cancelled)
+            {
+                this.DialogResult = DialogResult.Abort;
+                this.Close();
+            }
+            else
+            {
+                this.progressBar.Value = 0;
+                this.progressBarAll.Value++;
+                if (this.count < files.Count)
+                {
+                    // Set the temp file name and create new 0-byte file
+                    files[count].TempFile = Path.GetTempFileName();
+
+                    // Set up WebClient to download file
+                    webClient = new WebClient();
+                    webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(webClient_DownloadProgressChanged);
+                    webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(webClient_DownloadFileCompleted);
+
+                    // Set up backgroundworker to hash file
+                    bgWorker = new BackgroundWorker();
+                    bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+                    bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_RunWorkerCompleted);
+
+                    // Download file
+                    try { webClient.DownloadFileAsync(new Uri(files[count].Url), files[count++].TempFile); }
+                    catch { this.DialogResult = DialogResult.No; this.Close(); }
+                }
+                else
+                {
+                    // Show the "Hashing" label and set the progressbar to marquee
+                    this.lblProgress.Text = "Verifying Download...";
+                    this.progressBar.Style = ProgressBarStyle.Marquee;
+                    this.count = 0;
+
+                    // Start the hashing
+                    bgWorker.RunWorkerAsync(new string[] { this.files[count].TempFile, this.files[count++].Md5 });
+                }
+            }
+        }
+
+        /// <summary>
+        /// Formats the byte count to closest byte type
+        /// </summary>
+        /// <param name="bytes">The amount of bytes</param>
+        /// <param name="decimalPlaces">How many decimal places to show</param>
+        /// <param name="showByteType">Add the byte type on the end of the string</param>
+        /// <returns>The bytes formatted as specified</returns>
+        private string FormatBytes(long bytes, int decimalPlaces, bool showByteType)
+        {
+            double newBytes = bytes;
+            string formatString = "{0";
+            string byteType = "B";
+
+            // Check if best size in KB
+            if (newBytes > 1024 && newBytes < 1048576)
+            {
+                newBytes /= 1024;
+                byteType = "KB";
+            }
+            else if (newBytes > 1048576 && newBytes < 1073741824)
+            {
+                // Check if best size in MB
+                newBytes /= 1048576;
+                byteType = "MB";
+            }
+            else
+            {
+                // Best size in GB
+                newBytes /= 1073741824;
+                byteType = "GB";
+            }
+
+            // Show decimals
+            if (decimalPlaces > 0)
+                formatString += ":0.";
+
+            // Add decimals
+            for (int i = 0; i < decimalPlaces; i++)
+                formatString += "0";
+
+            // Close placeholder
+            formatString += "}";
+
+            // Add byte type
+            if (showByteType)
+                formatString += byteType;
+
+            return String.Format(formatString, newBytes);
+        }
+
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string file = ((string[])e.Argument)[0];
+            string updateMD5 = ((string[])e.Argument)[1];
+
+            // Hash the file and compare to the hash in the update xml
+            if (Hasher.HashFile(file, HashType.MD5) != updateMD5)
+                e.Result = DialogResult.No;
+            else
+                e.Result = DialogResult.OK;
+        }
+
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((DialogResult)e.Result != DialogResult.OK)
+            {
+                this.DialogResult = (DialogResult)e.Result;
+                this.Close();
+            }
+            else
+            {
+                if (count < files.Count)
+                    bgWorker.RunWorkerAsync(new string[] { this.files[count].TempFile, this.files[count++].Md5 });
+                else
+                {
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+            }
+        }
+
+        private void SharpUpdateDownloadForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (webClient.IsBusy)
+            {
+                webClient.CancelAsync();
+                this.DialogResult = DialogResult.Abort;
+            }
+
+            if (bgWorker.IsBusy)
+            {
+                bgWorker.CancelAsync();
+                this.DialogResult = DialogResult.Abort;
+            }
+        }
+    }
+}
---
+++ SharpUpdater.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace SharpUpdate
+{
+    /// <summary>
+    /// Provides application update support in C#
+    /// </summary>
+    public class SharpUpdater
+    {
+        /// <summary>
+        /// Holds the program-to-update's info
+        /// </summary>
+        private ISharpUpdatable applicationInfo;
+
+        /// <summary>
+        /// Thread to find update
+        /// </summary>
+        private BackgroundWorker bgWorker;
+
+        /// <summary>
+        /// Creates a new SharpUpdater object
+        /// </summary>
+        /// <param name="applicationInfo">The info about the application so it can be displayed on dialog boxes to user</param>
+        public SharpUpdater(ISharpUpdatable applicationInfo)
+        {
+            this.applicationInfo = applicationInfo;
+
+            // Set up backgroundworker
+            this.bgWorker = new BackgroundWorker();
+            this.bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+            this.bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_RunWorkerCompleted);
+        }
+
+        /// <summary>
+        /// Checks for an update for the program passed.
+        /// If there is an update, a dialog asking to download will appear
+        /// </summary>
+        public void DoUpdate()
+        {
+            if (!this.bgWorker.IsBusy)
+                this.bgWorker.RunWorkerAsync(this.applicationInfo);
+        }
+
+        /// <summary>
+        /// Checks for/parses update.xml on server
+        /// </summary>
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ISharpUpdatable application = (ISharpUpdatable)e.Argument;
+
+            // Check for update on server
+            if (!SharpUpdateXml.ExistsOnServer(application.UpdateXmlLocation))
+                e.Cancel = true;
+            else // Parse update xml
+                e.Result = SharpUpdateXml.Parse(application.UpdateXmlLocation, application.ApplicationID);
+        }
+
+        /// <summary>
+        /// Get the drirectory of the application
+        /// </summary>
+        /// <returns>Full path to the application to update</returns>
+        private string AssemblyDirectory()
+        {
+            string codeBase = this.applicationInfo.ApplicationAssembly.CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+
+        /// <summary>
+        /// After the background worker is done, prompt to update if there is one
+        /// </summary>
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // If there is a file on the server
+            if (!e.Cancelled)
+            {
+                SharpUpdateXml update = (SharpUpdateXml)e.Result;
+
+                // Check if the update is not null and is a newer version than the current application
+                if (update != null)
+                {
+                    //Check for missing files
+                    if(!update.HasAllFiles(AssemblyDirectory()))
+                        this.DownloadUpdate(update);
+                    else if (update.IsNewerThan(this.applicationInfo.ApplicationAssembly.GetName().Version))
+                    {
+                        // Ask to accept the update
+                        if (new SharpUpdateAcceptForm(this.applicationInfo, update).ShowDialog(this.applicationInfo.Context) == DialogResult.Yes)
+                            this.DownloadUpdate(update); // Do the update
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Downloads update and installs the update
+        /// </summary>
+        /// <param name="update">The update xml info</param>
+        private void DownloadUpdate(SharpUpdateXml update)
+        {
+            SharpUpdateDownloadForm form = new SharpUpdateDownloadForm(update.UpdateFiles, this.applicationInfo.ApplicationIcon);
+            DialogResult result = form.ShowDialog(this.applicationInfo.Context);
+
+            // Download update
+            if (result == DialogResult.OK)
+            {
+                string currentPath = this.applicationInfo.ApplicationAssembly.Location;
+                // "Install" it
+                UpdateApplication(form.TempFilesPath, currentPath, update.LaunchArgs);
+
+                Application.Exit();
+            }
+            else if (result == DialogResult.Abort)
+            {
+                MessageBox.Show("The update download was cancelled.\nThis program has not been modified.", "Update Download Cancelled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("There was a problem downloading the update.\nPlease try again later.", "Update Download Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// <summary>
+        /// Hack to close program, delete original, move the new one to that location
+        /// </summary>
+        /// <param name="tempFilePath">The temporary file's path</param>
+        /// <param name="currentPath">The path of the current application</param>
+        /// <param name="launchArgs">The launch arguments</param>
+        private void UpdateApplication(List<SharpUpdateFileInfo> tempFilePath, string currentPath, string launchArgs)
+        {
+            //Wait 4 seceonds to close the application
+            string argument = "/C choice /C Y /N /D Y /T 4 & ";
+
+            //Delete all file that will be updated
+            foreach (SharpUpdateFileInfo fi in tempFilePath)
+            {
+                argument += string.Format("Del /F /Q \"{0}\" & ", Path.GetDirectoryName(currentPath) + "\\" + fi.FileName);
+            }
+
+            //Wait 2 seconds
+            argument += "choice /C Y /N /D Y /T 2 & ";
+
+            //Move all downloaded files from temporarily path to installed path 
+            foreach (SharpUpdateFileInfo fi in tempFilePath)
+            {
+                argument += string.Format("Move /Y \"{0}\" \"{1}\" & ", fi.TempFile, Path.GetDirectoryName(currentPath) + "\\" + fi.FileName);
+            }
+
+            //Start the application after updateing all files
+            argument += "Start \"\" /D \"{0}\" \"{1}\" {2}";
+
+            //Start the process to do above tasks
+            ProcessStartInfo Info = new ProcessStartInfo();
+            Info.Arguments = String.Format(argument, Path.GetDirectoryName(currentPath), Path.GetFileName(tempFilePath[0].FileName), launchArgs);
+            Info.WindowStyle = ProcessWindowStyle.Hidden;
+            Info.CreateNoWindow = true;
+            Info.FileName = "cmd.exe";
+            Process.Start(Info);
+        }
+    }
+}
